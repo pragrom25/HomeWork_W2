4db5e69183af00b800aff81d4eb7748167881184
Rindaman <ppd.ing25@gmail.com>
2019-08-24 17:52:23 +0700

    .

--- app/src/main/java/com/example/homework_w2/MainActivity.java
+++ app/src/main/java/com/example/homework_w2/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.homework_w2;
 
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast t = Toast.makeText(MainActivity.this,"Welcome to My ToDo application!",Toast.LENGTH_SHORT);
+        t.show();
     }
 }
